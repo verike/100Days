05f3df232af0fe915e84d2939d13fcefdda0cfec
Victor Erike <verike7@gmail.com>
2022-09-16 13:46:25 +0100

    Day 2: I understood the concept of adding the unit of a two digit number by accessing the units variable subscripts or using the position index.

--- variables.py
+++ variables.py
@@ -1,23 +1,33 @@
 # Variable value switching test
 
-a = input("a: ")
-b = input("b: ")
+# a = input("a: ")
+# b = input("b: ")
 
-c = a
-a = b
-b = c
+# c = a
+# a = b
+# b = c
 
-print("a="+a)
-print("b="+b)
+# print("a="+a)
+# print("b="+b)
 
 # Band Name Generator
  
 welcome_text = 'Welcome to the Band Name Generator.'
 
-print(welcome_text)
+# print(welcome_text)
 
-city_name = input('What\'s name of the city you grew up in?\n')
+# city_name = input('What\'s name of the city you grew up in?\n')
 # print(city_name)
-pets_name = input('What\'s your pet\'s name?\n')
+# pets_name = input('What\'s your pet\'s name?\n')
 # print(pets_name)
-print(f'Your band name could be {city_name} {pets_name}')
+# print(f'Your band name could be {city_name} {pets_name}')
+
+
+# For adding the units of a two digit number 
+
+two_digit_numb = input('Enter a two digit number: ')
+first_dig = two_digit_numb[0]
+second_dig = two_digit_numb[1]
+
+print(int(first_dig) ** int(second_dig))
+
