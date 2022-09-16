774aceac28caa3e4225f118f77b90f1eec9a819b
Victor Erike <verike7@gmail.com>
2022-09-16 14:25:07 +0100

    Day 2: Understood the order of precedence of operation.

---
+++ math_ops.py
@@ -0,0 +1,21 @@
+4 + 5
+3 - 3
+2 * 4
+1 / 6
+5 ** 8
+10 % 2
+
+# Order of operational precedence in Python
+
+# PEDMAS BODMAS PEMDASLR
+# |
+# V
+# Bracket/Parenthesis ()
+# Exponential or Power to **
+# Multiplication *
+# Division /
+# Addition +
+# Subtraction -
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
