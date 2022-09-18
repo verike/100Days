72d4d39c8b0f1b5c1ef74d9d160b72bfb4fd0ff8
Victor Erike <verike7@gmail.com>
2022-09-19 00:52:05 +0100

    Day 6: Updated the calculator to print out statements to the users based on some conditions that were set with the 'Elif' Statement

--- BMIcal.py
+++ BMIcal.py
@@ -5,5 +5,17 @@ h = float(height)
 w = float(weight)
 # 
 BMI = (w / (h ** 2))
+
+if BMI < 18.5:
+    print(f'Your BMI is {BMI}, You\'re Underweight.')
+elif BMI < 25:
+    print(f'Your BMI is {BMI}, You have a Normal weight.')
+elif BMI < 30:
+    print(f'Your BMI is {BMI}, You\'re Overweight.')
+elif BMI < 35:
+    print(f'Your BMI is {BMI}, You\'re Obese.')
+else:
+    print(f'Your BMI is {BMI}, You\'re Clinically Obese.')
+
 print(BMI)
-print(f'Approximately {int(BMI)}')
+print(f'Approximately {round(BMI)}')
