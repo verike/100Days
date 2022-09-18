1e7b0f61e583659e3613a6f5e1177f1ebf2e3dcd
Victor Erike <verike7@gmail.com>
2022-09-19 00:31:31 +0100

    Day 5: Used the nested If statement and the Elif statement to test for multiple conditions

--- controlFlow.py
+++ controlFlow.py
@@ -1,23 +1,31 @@
-# Conditional Operators
+# Conditional Operators (If Statements & nested If statements)
 
-# print('Welcome to a roller coaster')
-# height = int(input('What is your height in cm? '))
+print('Welcome to a roller coaster')
+height = int(input('What is your height in cm? '))
 
-# if height >= 120:
-#     print('You can ride the roller coaster')
-# else:
-#     print('You can not ride the roller coaster, because you\'re not yet tall.')
+if height >= 120:
+    print('You can ride the roller coaster!')
+    age = int(input('Enter your age: '))
+
+    if age < 12:
+        print('Please pay $5.')
+    elif age <= 18:
+        print('Please pay $7.')
+    else:
+        print('please pay $12.')
+else:
+    print('You can not ride the roller coaster, because you\'re not yet tall.')
 
 
 # Check if a number is Odd or Even
 
-print('Welcome to number checker')
+# print('Welcome to number checker')
 
-number = int(input('Enter a number: '))
+# number = int(input('Enter a number: '))
 
-test = number % 2
+# test = number % 2
 
-if test == 0:
-    print('This is an Even Number')
-else:
-    print('This is an Odd Number')
+# if test == 0:
+#     print('This is an Even Number')
+# else:
+#     print('This is an Odd Number')
