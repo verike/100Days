080081cbbe022a9a37c7da00971e2b22a9e0008b
Victor Erike <verike7@gmail.com>
2022-09-18 23:53:37 +0100

    Day 5: Used the If and Else statement to check for a simple condition

---
+++ controlFlow.py
@@ -0,0 +1,23 @@
+# Conditional Operators
+
+# print('Welcome to a roller coaster')
+# height = int(input('What is your height in cm? '))
+
+# if height >= 120:
+#     print('You can ride the roller coaster')
+# else:
+#     print('You can not ride the roller coaster, because you\'re not yet tall.')
+
+
+# Check if a number is Odd or Even
+
+print('Welcome to number checker')
+
+number = int(input('Enter a number: '))
+
+test = number % 2
+
+if test == 0:
+    print('This is an Even Number')
+else:
+    print('This is an Odd Number')
